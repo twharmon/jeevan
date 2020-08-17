33f3a23d99a3a928471dc5dd77d9da6969940eef
Travis Harmon <twharmon@gmail.com>
2020-08-17 15:38:28 -0500

    Add to HttpStatus enum

--- src/response.ts
+++ src/response.ts
@@ -4,6 +4,8 @@ import * as fs from 'fs'
 export enum HttpStatus {
     OK = 200,
     BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
     NotFound = 404,
     Conflict = 409,
     UnprocessableEntity = 422,
